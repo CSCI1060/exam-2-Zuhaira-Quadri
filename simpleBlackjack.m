852a66243056a3e1a44fdab88aeb4e7391c2d894
Zuhaira-Quadri <zuhaira.quadri@slu.edu>
2024-11-10 23:26:28 -0600

    Update simpleBlackjack.m

--- simpleBlackjack.m
+++ simpleBlackjack.m
@@ -13,3 +13,21 @@ function avgScore = simpleBlackjack(limit, trials)
 % OUTPUT avgScore – The average final score of
 % all hands.
 
+function avgScore = simpleBlackjack(limit, trials)
+    % Card values, with face cards as 10
+    cards = [2 3 4 5 6 7 8 9 10 10 10 10];
+    totalScore = 0;
+
+    for t = 1:trials
+        handTotal = 0;
+        
+        while handTotal < limit
+            card = cards(randi(12));  
+            handTotal = handTotal + card;
+        end
+        
+        totalScore = totalScore + handTotal;  
+    end
+    
+    avgScore = totalScore / trials;  
+end
